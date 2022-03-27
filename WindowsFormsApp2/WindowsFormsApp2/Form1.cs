33a47a27a292b01f4c22063f8be2450a50636bc2
JKiblik <102478047+JKiblik@users.noreply.github.com>
2022-03-27 19:03:42 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "4000";
+            textBox2.Text = "-0,875";
+            textBox3.Text = "-0,000475";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            textBox4.Text += Environment.NewLine + "x = " + x.ToString();
+            double y = double.Parse(textBox2.Text);
+            textBox4.Text += Environment.NewLine + "y = " + y.ToString();
+            double z = double.Parse(textBox3.Text);
+            textBox4.Text += Environment.NewLine + "z = " + z.ToString();
+            double a = Math.Pow((Math.Abs(Math.Cos(x)-Math.Cos(y))),(1 + 2 * Math.Pow(Math.Sin(y),2)));
+            double b = 1 + z + (Math.Pow(z, 2) / 2) + (Math.Pow(z, 3) / 3) + (Math.Pow(z, 4) / 4);
+            double w = a * b;
+            textBox4.Text += Environment.NewLine + "w =" + w.ToString();
+        }
+    }
+}
